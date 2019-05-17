7cfa40a83dc7f68eeda4a04e91b08980980cb6c8
AustinBH <harlowaustin2@gmail.com>
2019-05-17 13:02:14 -0700

    Still working

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,28 @@
 class Manager
 
+  attr_reader :name, :department, :age, :employees
+
+  @@managers = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @employees = []
+    @@managers << self
+  end
+
+  def self.all
+    @@managers
+  end
+
+  def hire_employees(name, salary)
+    new_hire = Employee.new(name, salary)
+    @employees << new_hire
+  end
+
+  def self.all_departments
+    self.all.map{|info| info}
+  end
+
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,8 @@ require 'pry'
 
 
 #Test your code here
+e1 = Manager.new("Steve", "Housekeeping", 25)
+puts Manager.all_departments
 
 
 binding.pry
