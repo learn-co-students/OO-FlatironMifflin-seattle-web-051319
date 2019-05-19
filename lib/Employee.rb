7c400b4057b2f630d2a120ab034be2d1ab4eb4b3
jenshaw86 <bloo.j.shaw@gmail.com>
2019-05-19 12:23:11 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,41 @@
+require "pry"
 class Employee
+    attr_accessor :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(num)
+        # return array of all employees with salaries over num
+        employees = @@all.select {|employee| employee.salary > num}
+        employees.collect {|employee| employee.name}
+    end
+
+    def self.find_by_department(dept)
+        manager_of_dept = Manager.all.find {|manager| manager.dept == dept}
+        manager_of_dept.employees.first
+        # returns FIRST employee whose manager works in dept
+        # HINT: .find
+    end
+
+    def tax_bracket
+        # returns array of all employees whose salaries are less than +-1000 of employee who calls method
+        people = @@all.select do |employee| 
+            salary_dif = (employee.salary - self.salary).abs
+            salary_dif <= 1000
+        end
+        people.each {|person| puts person.name}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,24 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+jenny = Manager.new("Jenny", "Evil", 32)
+aidi = Manager.new("Aidi", "Purgatory", 26)
+
+# jenny hires employees and pays salary in bananas
+jenny.hire_employee("Fabian", 100000)
+jenny.hire_employee("Noah", 250000)
+jenny.hire_employee("Austin", 80000)
+jenny.hire_employee("Aliyah", 80500)
+jenny.hire_employee("David", 79800)
+
+chris = Employee.new("Christopher", 80200, jenny)
+
+puts chris.tax_bracket
+# aidi hires a minion and pays salary in melons
+aidi.hire_employee("Chet", 500)
 
 
-binding.pry
+
+# puts Employee.paid_over(500)
+# binding.pry
 puts "done"
