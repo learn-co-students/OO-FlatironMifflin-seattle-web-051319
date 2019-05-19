fc662f1c5a020831be7663e8a27f360dacf3a7d4
github username <github email address>
2019-05-19 12:19:22 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,50 @@
 class Employee
+attr_reader :name, :salary
+attr_accessor :manager
+@@all = []
 
+def initialize(name, salary, manager)
+  @name = name
+  @salary = salary
+  @manager = manager
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def manager_name
+  @manager.name
+end
+
+def self.paid_over(num)
+  # binding.pry
+  self.all.select do |employee|
+    employee.salary > num
+    # binding.pry
+  end
+end
+
+def self.find_by_department(department_name)
+  # binding.pry
+  manager_of_department = Manager.all.find do |manager|
+    manager.department == department_name
+    # manager.employees[0]
+  end
+  manager_of_department.employees.first
+end
+
+def tax_bracket
+  people = @@all.select do |employee|
+    salary_diff = (self.salary - employee.salary).abs
+    salary_diff <= 1000
+    # binding.pry
+  end
+  # binding.pry
+  people.each do |person|
+    puts person.name
+  end
+  end
+  # binding.pry
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,41 @@
+# require 'pry'
+
 class Manager
+attr_reader :name, :department, :age, :employees
+@@all = []
+
+def initialize(name, department, age)
+  @name = name
+  @department = department
+  @age = age
+  @employees = []
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def hire_employee(name, salary)
+  new_employee = Employee.new(name, salary, self)
+  # new_employee.manager == self
+  @employees << new_employee
+end
+
+def self.all_departments
+  @@all.collect do |manager|
+    manager.department
+  end
+end
+
+def self.average_age
+  # self.average(manager.age)
+  all_ages = @@all.collect do |manager|
+    manager.age
+  end
+  number_of_managers = @@all.size
+  # binding.pry
+  all_ages.sum/number_of_managers.to_f
+end
 
 end
